63877a277edbd32c0887ec5458fd6905349d01a0
jianghao <jianghao@ipin.com>
2018-07-07 11:26:55 +0800

    update test

--- src/main/java/com/proxy2808/client/Client.java
+++ src/main/java/com/proxy2808/client/Client.java
@@ -270,9 +270,12 @@ public class Client {
     }
 
     public static void main(String[] args) throws Exception {
-        String username = "daoxu_test";
-        String password = "987654789DXH";
+        String username = "xxxx";
+        String password = "xxxx";
         Client client = new Client(username, password);
-        client.releaseAll();
+        Result<List<Proxy>> proxies = client.getProxy(1, 10L); // 获取1个代理，过期时间为10秒
+        client.releaseProxies(proxies.getData());  // 释放代理
+        proxies = client.listProxy(); // 列出所有已经获取到的代理
+        client.releaseAll(); // 释放所有已经获取到的代理
     }
 }
